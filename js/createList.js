04539083b267186c445cc668963511ae3b34aefd
otknoy <otknoy@gmail.com>
2014-09-20 14:54:02 +0900

    ボタンを押す度にリストがリセットされるようにした

--- js/createList.js
+++ js/createList.js
@@ -1,25 +1,25 @@
 var xmlFiles = new Array("981114074.xml","981115231.xml","981125080.xml");
 $(function(){
-    // カウンターを生成
-    var counter = 0;
-    var j = 0;
     // ボタンがクリックされた時の処理
     $(":button").click(function() {
-        if(counter == 0){
-            for(var i=0; i<xmlFiles.length; i++){
-                // 「data.xml」を読み込む
-                $.get("./data/" + xmlFiles[i],
-                    {},
-                    createList,
-                    "xml"
-                );
-            }
-            counter = 1; // 一度だけ実行させる
+        for(var i=0; i<xmlFiles.length; i++){
+	    // リストの要素を空に
+	    $('#list').empty();
+
+            // 「data.xml」を読み込む
+            $.get("./data/" + xmlFiles[i],
+                  {},
+                  createList,
+                  "xml"
+                 );
         }
     });
-
+    
     // 表を作成する関数
     var createList = function(xml) {
+	// カウンターを生成
+	var j = 0;
+
         // datasetタグの要素を繰り返し処理する
         $(xml).find("DOC").each(function(){
 
